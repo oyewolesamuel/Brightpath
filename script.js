44de0a9fdd2eb6bad6cd1f3f7a3f1c47d6c67f31
Samuel Oyewole <oyewolesamuel632@gmail.com>
2025-11-07 11:10:02 +0100

    frist comment

---
+++ script.js
@@ -0,0 +1,7 @@
+//DOM
+const hamburger = document.querySelector(".hamburger")
+const sidebar = document.querySelector(".side-bar")
+
+hamburger.addEventListener("click" , function(){
+    sidebar.classList.toggle("active")
+})
